eff43b8c59d063d527418c18bbb2fb8203e1e952
edithv <edithv@users.noreply.github.com>
2015-11-20 12:24:00 -0500

    aventure 2 init commit

---
+++ PlaneteExtraordinaire/Aventure_2.m
@@ -0,0 +1,91 @@
+% LES BOUCLES FOR
+
+% Le mot 'for' est le mot anglais se traduisant par 'pour'. Supposons que je
+% désire programmer une boucle comptant de 1 à 5:
+
+n = 0;
+
+for i = 1:1:5
+
+	n = n + 1;
+
+end
+
+% En français, cela se traduit ainsi :
+
+% posons n = 0.
+
+% pour i allant de 1 jusqu'à 5, en faisant des sauts de 1
+
+%	additionne 1 à n
+
+% fin de la boucle pour.
+
+% Pourrais-tu m'aider à programmer une boucle comptant de 1 à 10 ?
+% Je suis certaine que tu es capable :)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+% SOLUTION
+
+% Cette solution n'est pas unique, mais c'est la plus simple.
+
+n = 0;
+
+for i = 1:1:10
+
+	n = n + 1;
+
+end
+
+% Par exemple, voici une autre solution.
+
+n = 0;
+
+for i = 1:1:5
+
+	n = n + 2;
+
+end
+
+
+
+
+
